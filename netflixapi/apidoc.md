84dd96bf1b0a36e7afeb94b0142991db1c73c1b9
siddharth mahobia <siddharthmahobia1@gmail.com>
2023-01-14 17:42:51 +0530

    first commit

---
+++ netflixapi/apidoc.md
@@ -0,0 +1,17 @@
+>list of category
+http://localhost:1000/category
+
+>list of records
+http://localhost:1000/records
+
+>records wrt category
+http://localhost:1000/category?categoryId=2
+
+>filter on basis of category + movie_type
+http://localhost:1000/filters/1?movietype=Hollywood
+
+>filter on basis of category + rating
+http://localhost:1000/filters/1?lrating=7%20&&%20hrating=8
+
+>detail of category
+http://localhost:1000/details/1
---
+++ netflixapi/app.js
@@ -0,0 +1,102 @@
+let express = require('express');
+let app = express();
+let port=1000;
+let cors=require('cors')
+let mongo =require('mongodb');
+let MongoClient=mongo.MongoClient;
+let mongoUrl="mongodb://localhost:27017";
+let db;
+
+//middleware
+app.use(cors())
+
+
+app.get('/', (req, res) =>{
+    res.send("<h1>hello from express</h1>");
+})
+
+
+
+
+app.get('/category',(req,res) => {
+    db.collection('category').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+ })
+
+
+
+
+
+ app.get('/records',(req,res) => {
+    let query={}
+    let categoryId = Number(req.query.categoryId);
+    
+    if(categoryId)
+    {
+        query={category_id:categoryId}
+    }
+
+    db.collection('records').find(query).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+ })
+
+
+
+
+
+ app.get('/filters/:categoryId',(req,res) => {
+    let query = {};
+    let categoryId= Number(req.params.categoryId);
+    let movieType = req.query.movieType;
+    let rating = Number(req.query.rating);
+    let lrating = Number(req.query.lrating);
+    let hrating = Number(req.query.hrating);
+
+    if(movieType)
+    {
+        query={
+            category_id:categoryId,
+            movie_type:movieType
+        }
+    }
+    else if(lrating && hrating)
+    {
+       query={
+            category_id:categoryId,
+           $and:[{rating:{$gt:lrating,$lt:hrating}}]
+        }
+    }
+    
+    db.collection('records').find(query).toArray((err,result) => {
+       if(err) throw err;
+       res.send(result)
+   })
+})
+
+
+
+
+app.get('/details/:categoryId',(req,res) => {
+    //let id = mongo.ObjectId(req.params.restId)
+    let categoryId = Number(req.params.categoryId)
+    db.collection('records').find({category_id:categoryId}).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+ 
+
+
+
+// connect with mongodb
+MongoClient.connect(mongoUrl,{useNewUrlParser:true},(err,dc) => {
+    if(err) console.log('Error while connecting');
+    db = dc.db('netflixdatabase');
+    app.listen(port,() => {
+        console.log(`Server is running on port ${port}`)
+    })
+})
